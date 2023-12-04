869cb0dd01e07eac07c3f98378da4f3cf5ffee55
Álvaro Rofa Aranda <91280550+Alvarapo@users.noreply.github.com>
2023-12-04 23:45:36 +0100

    mejoras

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,10 @@ function addToCart(money) {
     updateTotalMoney(money)
 }
 
-let totalMoney = 0.00; // Variable para almacenar el total del dinero
+let totalMoney = 0.00;
 const cartTotalElement = document.getElementById('cart-total');
 
 function updateTotalMoney(price) {
-    // Actualizar el total del dinero al añadir un producto
     totalMoney += price;
-    cartTotalElement.textContent = totalMoney.toFixed(2); // Ajustar el total a dos decimales
+    cartTotalElement.textContent = totalMoney.toFixed(2);
 }
